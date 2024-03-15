62e6da13a948d55ae8853eaac826c0519e280542
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-03-16 00:09:03 +0100

    Assignment 2 is finished.

---
+++ src/_03_CSSSelector/_02_Assignment.java
@@ -0,0 +1,26 @@
+package _03_CSSSelector;
+
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _02_Assignment extends BaseDriver {
+    @Test
+    public void test1(){
+        driver.get("https://www.snapdeal.com/");
+
+        WebElement searchBox = driver.findElement(By.cssSelector("[id='inputValEnter']"));
+        searchBox.sendKeys("teddy bear");
+
+        WebElement searchButton = driver.findElement(By.cssSelector("button[class='searchformButton col-xs-4 rippleGrey']"));
+        searchButton.click();
+
+        WebElement text = driver.findElement(By.cssSelector("[id='searchMessageContainer']"));
+
+        Assert.assertTrue("Text don't match",text.getText().equals("We've got 273 results for teddy bear"));
+
+        WaitClose();
+    }
+}
